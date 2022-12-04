b380b128c743847bb1d2d2d3a802f584216af223
Barış <barisayyildiz.1799@gmail.com>
2022-12-04 15:43:10 +0300

    model predict yapıyor

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-from utils import audio_to_spec
+from utils import audio_to_spec, get_loaded_model_by_name, load_data_from_generators, predict_spec, remove_file
 
 import os
 import uuid
+
+# from tensorflow.keras.optimizers import SGD
+# from keras.preprocessing.image import ImageDataGenerator
+
 
 class Item(BaseModel):
 	name: str
@@ -58,17 +62,23 @@ async def anaylze_sound(sound: UploadFile = File()):
 
 	print(sound.file)
 
-	rnd_id = str(uuid.uuid4()) + ".wav"
+	rnd_id = str(uuid.uuid4())
 
 	root_dir = "audios"
-	path = os.path.join(root_dir, rnd_id)
+	path = os.path.join(root_dir, rnd_id + ".wav")
 	# print(path)
 
 	with open(path, mode="wb") as f:
 		f.write(content_sound)
 	audio_to_spec(path)
+	prediction = predict_spec()
 
-	return {"Hello": "World"}
+	remove_file(rnd_id)
+
+
+
+	return {"vowel" : prediction}
+	# return {"Hello": "World"}
 
 @app.post("/save_upload_file_tmp")
 def save_upload_file_tmp(file: UploadFile) -> Path:
@@ -82,17 +92,6 @@ def save_upload_file_tmp(file: UploadFile) -> Path:
 	return {"file_name":file.filename}
 
 
-	# try:
-	# 	suffix = Path(upload_file.filename).suffix
-	# 	with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
-	# 		shutil.copyfileobj(upload_file.file, tmp)
-	# 		tmp_path = Path(tmp.name)
-	# finally:
-	# 	print("hello")
-	# 	upload_file.file.close()
-	# return tmp_path
-
-
 @app.post("/items/")
 async def create_item(item: Item):
 	print(item)
@@ -103,6 +102,24 @@ async def create_item(item: Item):
 	return item_dict
 
 
-files = [i for i in os.listdir() if os.path.isfile(i)]
-print(files)
+# def main():
+# 	filename ="vowels_spec_model"
+# 	model_s = get_loaded_model_by_name(filename)
+# 	opt = SGD(lr=0.001)
+# 	model_s.compile(loss = "categorical_crossentropy", optimizer =opt,metrics=['accuracy'], run_eagerly=True)
+
+# 	print(model_s)
+
+# 	################ Spectrogram images ##############
+# 	h = 96
+# 	w = 96
+
+# 	path_spec_test = "specs"
+# 	spec_datagen_test = ImageDataGenerator()
+
+# 	test_generator_spec = spec_datagen_test.flow_from_directory(path_spec_test,class_mode='categorical',color_mode ='rgb',shuffle = False,  target_size =(h,w),subset='training')
+# 	test_spec_data,test_labels_spec = load_data_from_generators(test_generator_spec,timestep=1,color_mode='rgb')
+
+
+# main()
 
--- utils.py
+++ utils.py
@@ -3,6 +3,18 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
+import os
+
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.optimizers import SGD
+
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+# from keras.preprocessing.image import array_to_img, img_to_array, load_img
+from tensorflow.keras.utils import img_to_array,load_img,array_to_img
+
+from keras.preprocessing.image import DirectoryIterator,DataFrameIterator
+
 def audio_to_spec(path: str):
 	file_name = path.split("/")[-1]
 
@@ -13,4 +25,63 @@ def audio_to_spec(path: str):
 	plt.figure(figsize=(8,8))
 
 	plt.specgram(y, NFFT=2048, Fs=2, Fc=0, noverlap=128, cmap=cmap, sides='default', mode='default', scale='dB')
-	plt.savefig(f'specs/{file_name}.png')
+	plt.savefig(f'specs/images/{file_name.split(".")[0]}.png')
+
+
+def get_loaded_model_by_name(filename):
+  # load json and create model
+  json_file = open(f'models/{filename}.json', 'r')
+  loaded_model_json = json_file.read()
+  json_file.close()
+  loaded_model = model_from_json(loaded_model_json)
+  # load weights into new model
+  loaded_model.load_weights(f'models/{filename}.h5')
+  print("Loaded model from disk")
+  # print(loaded_model.summary())
+  return loaded_model
+
+
+def load_data_from_generators(generator,timestep,color_mode ='grayscale',target_size =(96,96)):
+	data_frames = []
+	dat_labels = []
+
+	for i in range(0,len(generator.filepaths),timestep):
+		time_dist_data = []
+		for k in range(timestep):
+			time_dist_data.append(img_to_array(load_img(generator.filepaths[i+k],color_mode=color_mode,target_size=target_size)))
+		dat_labels.append(generator.labels[i])
+		data_frames.append(time_dist_data)
+
+	return np.array(data_frames),np.array(dat_labels)
+
+
+def predict_spec():
+	filename ="vowels_spec_model"
+	model_s = get_loaded_model_by_name(filename)
+	opt = SGD(lr=0.001)
+	model_s.compile(loss = "categorical_crossentropy", optimizer =opt,metrics=['accuracy'], run_eagerly=True)
+
+	spec_datagen = ImageDataGenerator()
+	
+	h = 96
+	w = 96
+
+	path_spec_test = "specs"
+
+	spec_datagen_test = ImageDataGenerator()
+	test_generator_spec = spec_datagen_test.flow_from_directory(path_spec_test,class_mode='categorical',color_mode ='rgb',shuffle = False,  target_size =(h,w),subset='training')
+	test_spec_data,test_labels_spec = load_data_from_generators(test_generator_spec,timestep=1,color_mode='rgb')
+
+	vowels = ["a", "e", "ii", "i", "o", "oo", "u", "uu"]
+	prediction = model_s.predict(test_spec_data)[0]
+
+	print(model_s.predict(test_spec_data))
+	print(prediction)
+
+	return vowels[np.argmax(prediction)]
+
+
+
+def remove_file(id):
+	os.remove(f'audios/{id}.wav')
+	os.remove(f'specs/images/{id}.png')
